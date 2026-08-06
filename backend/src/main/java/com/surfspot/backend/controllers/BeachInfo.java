072611bba21bcedf1f31d24fcd5ad06f40c9ed8b
Marcus Connelly <marcus.connelly@hotmail.com>
2026-08-06 16:05:24 +0100

    swapped everything to Java Record to return JSON objects, and added fake data for 3 beaches

--- backend/src/main/java/com/surfspot/backend/controllers/BeachInfo.java
+++ backend/src/main/java/com/surfspot/backend/controllers/BeachInfo.java
@@ -1,3 +1,3 @@
 package com.surfspot.backend.controllers;
 
-public record BeachInfo (String beachName, int waterTemp) {}
+public record BeachInfo (String beachName, String forecastDateTime, double waveHeight, int wavePeriod, double windSpeed,String windDirection, double currentTideHeight, double airTemp, double waterTemp, String weatherCondition, String sunrise, String sunset) {}
