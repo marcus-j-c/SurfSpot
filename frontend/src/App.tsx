78202c85517b05e7b3e60a91ceda078548130b07
Marcus Connelly <marcus.connelly@hotmail.com>
2026-08-06 21:51:09 +0100

    first typescript written and tested my first html tags in app.tsx, first colours also added using app.css

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,16 @@
+import './App.css';
+
 function App() {
   return (
     <>
-      Hello World!
+    <div className = "welcome">
+      <h1>Hello Surfspot!</h1>
+      <p>Welcome to Surfspot!</p>
+    </div>
+    <button>Click me</button>
+    <input type = "text" style = {{width: "400px"}} placeholder = "Test text entry box, enter whatever you want!!"/>
     </>
-  )
+  );
 }
 
 export default App
