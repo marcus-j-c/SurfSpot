1c37769095b19b7b1bc837989a35f53290286125
Marcus Connelly <marcus.connelly@hotmail.com>
2026-08-07 16:15:48 +0100

    First props used & devlog updates

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,7 +7,24 @@ function App() {
     <>
     <div className = "welcome">
       <h1>Hello Surfspot!</h1>
-      <ForecastCard/>
+      <ForecastCard 
+        beachName = "Bonzai Pipeline"
+        waveHeight = {3.8}
+        windDirection ="ENE"
+        airTemp = {28.0}
+      />
+      <ForecastCard
+        beachName = "Bells Beach"
+        waveHeight = {2.3}
+        windDirection = "W"
+        airTemp = {15.0}
+        />
+        <ForecastCard
+        beachName = "Jeffreys Bay"
+        waveHeight = {1.6}
+        windDirection = "SW"
+        airTemp = {19.0}
+        />
       <SearchBar/>
     </div>
     </>
