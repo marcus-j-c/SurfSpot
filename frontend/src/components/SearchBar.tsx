9cf93ebb8d98d4da5dab635857ad03514811a150
Marcus Connelly <marcus.connelly@hotmail.com>
2026-08-07 17:01:34 +0100

    First use of useState, onChange, and onClick

--- frontend/src/components/SearchBar.tsx
+++ frontend/src/components/SearchBar.tsx
@@ -1,8 +1,15 @@
+import {useState} from 'react';
+
 export function SearchBar() {
+    const [typedText, setTypedText] = useState(""); //this  variable holds what the user types
+    const handleSearch = () => alert("You searched for: " + typedText); //triggers when user hits button
+
     return (
         <div className="search-bar">
-            <input type = "text" style = {{width:"400px"}} placeholder = "Enter a beach name..."/>
-            <button>Search</button>
+            <input type = "text" style = {{width:"400px"}} placeholder = "Enter a beach name..." onChange = {(e) => setTypedText(e.target.value) /*updates typedText instantly*/}/>
+            <button onClick = {handleSearch}>Search</button>
+            {/*show live updates to typedText */}
+            <p>Current state of typedText: {typedText}</p>
         </div>
     );
 }
