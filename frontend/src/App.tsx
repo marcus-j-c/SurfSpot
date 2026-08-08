d6cc8579d7535185aeea7b0a7632215838810a55
Marcus Connelly <marcus.connelly@hotmail.com>
2026-08-08 20:19:05 +0100

    search box looks amazing and is in the right place!

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -14,6 +14,13 @@ function App() {
   return (
     <>
     <div className = "Overall-Grid">
+      <div className="box-1">Box 1</div> {/*temporary classes for grid*/}
+      <div className="box-2">Box 2</div>
+      <div className="box-3">Box 3</div>
+      <div className="box-4">Box 4</div>
+      <div className="box-5">Box 5</div>
+      <div className="box-6">Box 6</div>
+      <div className="box-7">Box 7</div>
     {/*<div className = "welcome">
       <h1>Hello Surfspot!</h1>
       <ForecastCard 
