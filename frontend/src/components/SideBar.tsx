ba38c42eee80987e8eba68dae2cd1b63c49bc6f1
Marcus Connelly <marcus.connelly@hotmail.com>
2026-08-10 17:11:20 +0100

    a bit more styling added to the sidebar

--- frontend/src/components/SideBar.tsx
+++ frontend/src/components/SideBar.tsx
@@ -7,11 +7,41 @@ export function SideBar() {
             <h1>Trending Beaches</h1>
             <nav>
                 <ul className = "side-bar-links">
-                <li><a href = "#">Beach1</a></li>
-                <li><a href = "#">Beach2</a></li>
-                <li><a href = "#">Beach3</a></li>
-                <li><a href = "#">Beach4</a></li>
-                <li><a href = "#">Beach5</a></li>
+                <li>
+                    <a href = "#" className = "trending-item">
+                        <span className = "beach-name">1. Bonzai Pipeline</span>
+                        <i className = "material-symbols-outlined icon positive">trending_up</i>
+                        <span className = "beach-change positive">+28%</span>
+                    </a>
+                </li>
+                <li>
+                    <a href = "#" className = "trending-item">
+                        <span className = "beach-name">2. Bells Beach</span>
+                        <i className = "material-symbols-outlined icon negative">trending_down</i>
+                        <span className = "beach-change negative">-15%</span>
+                    </a>
+                </li>
+                <li>
+                    <a href = "#" className = "trending-item">
+                        <span className = "beach-name">3. Jeffreys Bay</span>
+                        <i className = "material-symbols-outlined icon positive">trending_up</i>
+                        <span className = "beach-change positive">+43%</span>
+                    </a>
+                </li>
+                <li>
+                    <a href = "#" className = "trending-item">
+                        <span className = "beach-name">4. Teahupo'o</span>
+                        <i className = "material-symbols-outlined icon positive">trending_up</i>
+                        <span className = "beach-change positive">+5%</span>
+                    </a>
+                </li>
+                <li>
+                    <a href = "#" className = "trending-item">
+                        <span className = "beach-name">5. Supertubos</span>
+                        <i className = "material-symbols-outlined icon negative">trending_down</i>
+                        <span className = "beach-change negative">-2%</span>
+                    </a>
+                </li>
                 </ul>
             </nav>
         </div>
