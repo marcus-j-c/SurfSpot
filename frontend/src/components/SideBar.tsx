7fecc85480753c9e1912f02476dbbe06f331c0ee
Marcus Connelly <marcus.connelly@hotmail.com>
2026-08-11 17:04:37 +0100

    Sidebar done for now

--- frontend/src/components/SideBar.tsx
+++ frontend/src/components/SideBar.tsx
@@ -9,37 +9,102 @@ export function SideBar() {
                 <ul className = "side-bar-links">
                 <li>
                     <a href = "#" className = "trending-item">
-                        <span className = "beach-name">1. Bonzai Pipeline</span>
+                        <div className = "beach-info">
+                            <span className = "beach-name">1. Bonzai Pipeline</span>
+                            <span className = "beach-location">Hawaii, USA</span>
+                        </div>
                         <i className = "material-symbols-outlined icon positive">trending_up</i>
                         <span className = "beach-change positive">+28%</span>
                     </a>
                 </li>
                 <li>
                     <a href = "#" className = "trending-item">
-                        <span className = "beach-name">2. Bells Beach</span>
+                        <div className = "beach-info">
+                            <span className = "beach-name">2. Bells Beach</span>
+                            <span className = "beach-location">Victoria, Australia</span>
+                        </div>
                         <i className = "material-symbols-outlined icon negative">trending_down</i>
                         <span className = "beach-change negative">-15%</span>
                     </a>
                 </li>
                 <li>
                     <a href = "#" className = "trending-item">
-                        <span className = "beach-name">3. Jeffreys Bay</span>
+                        <div className = "beach-info">
+                            <span className = "beach-name">3. Jeffreys Bay</span>
+                            <span className = "beach-location">Eastern Cape, South Africa</span>
+                        </div>
                         <i className = "material-symbols-outlined icon positive">trending_up</i>
                         <span className = "beach-change positive">+43%</span>
                     </a>
                 </li>
                 <li>
                     <a href = "#" className = "trending-item">
-                        <span className = "beach-name">4. Teahupo'o</span>
+                        <div className = "beach-info">
+                            <span className = "beach-name">4. Teahupo'o</span>
+                            <span className = "beach-location">Tahiti, French Polynesia</span>
+                        </div>
                         <i className = "material-symbols-outlined icon positive">trending_up</i>
                         <span className = "beach-change positive">+5%</span>
                     </a>
                 </li>
                 <li>
                     <a href = "#" className = "trending-item">
-                        <span className = "beach-name">5. Supertubos</span>
+                        <div className = "beach-info">
+                            <span className = "beach-name">5. Supertubos</span>
+                            <span className = "beach-location">Peniche, Portugal</span>
+                        </div>
                         <i className = "material-symbols-outlined icon negative">trending_down</i>
                         <span className = "beach-change negative">-2%</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#" className="trending-item">
+                        <div className="beach-info">
+                            <span className="beach-name">6. Nazaré</span>
+                            <span className="beach-location">Leiria, Portugal</span>
+                        </div>
+                        <i className="material-symbols-outlined icon positive">trending_up</i>
+                        <span className="beach-change positive">+51%</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#" className="trending-item">
+                        <div className="beach-info">
+                            <span className="beach-name">7. Uluwatu</span>
+                            <span className="beach-location">Bali, Indonesia</span>
+                        </div>
+                        <i className="material-symbols-outlined icon positive">trending_up</i>
+                        <span className="beach-change positive">+19%</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#" className="trending-item">
+                        <div className="beach-info">
+                            <span className="beach-name">8. Gold Coast</span>
+                            <span className="beach-location">Queensland, Australia</span>
+                        </div>
+                        <i className="material-symbols-outlined icon negative">trending_down</i>
+                        <span className="beach-change negative">-8%</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#" className="trending-item">
+                        <div className="beach-info">
+                            <span className="beach-name">9. Mavericks</span>
+                            <span className="beach-location">California, USA</span>
+                        </div>
+                        <i className="material-symbols-outlined icon positive">trending_up</i>
+                        <span className="beach-change positive">+34%</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#" className="trending-item">
+                        <div className="beach-info">
+                            <span className="beach-name">10. Hossegor</span>
+                            <span className="beach-location">Landes, France</span>
+                        </div>
+                        <i className="material-symbols-outlined icon negative">trending_down</i>
+                        <span className="beach-change negative">-4%</span>
                     </a>
                 </li>
                 </ul>
