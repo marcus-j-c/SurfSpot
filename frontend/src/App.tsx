70afa3ed958a18961b0edb8251a77902de88f20b
Marcus Connelly <marcus.connelly@hotmail.com>
2026-08-12 18:39:00 +0100

    huge layout change, moved to react router, and now have a generic layout that applies to all pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,17 @@
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Home from "./pages/Home";
+import BeachDetail from "./pages/BeachDetail";
+import Layout from "./Layout";
 import "./App.css";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route element={<Layout />}>
+          <Route path = "/" element = {<Home/>}/>
+          <Route path = "spot/:beachName" element = {<BeachDetail/>}/>
+        </Route>
       </Routes>
     </BrowserRouter>
   );
