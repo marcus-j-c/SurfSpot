08bfdd5b038fc1cca58c9a1716f3985b9cad7a56
Marcus Connelly <marcus.connelly@hotmail.com>
2026-09-01 21:57:46 +0100

    ive got real data!! just gotta convert a few numbers into meaningfull text, then i can move onto the rating system!

--- backend/src/main/java/com/surfspot/backend/controllers/BackupGeocodingInfo.java
+++ backend/src/main/java/com/surfspot/backend/controllers/BackupGeocodingInfo.java
@@ -1,3 +1,3 @@
 package com.surfspot.backend.controllers;
 
-public record BackupGeocodingInfo(String name, String lat, String lon) { }
+public record BackupGeocodingInfo(String name, String lat, String lon, double importance) {}
