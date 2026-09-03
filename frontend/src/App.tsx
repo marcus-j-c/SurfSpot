69fa8453dee73e040ceabb046c8fd1e2dcd54229
Marcus Connelly <marcus.connelly@hotmail.com>
2026-09-03 18:54:45 +0100

    small changes + added 404 page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import Home from "./pages/Home";
 import BeachDetail from "./pages/BeachDetail";
 import Layout from "./Layout";
 import "./App.css";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route element={<Layout />}>
           <Route path = "/" element = {<Home/>}/>
           <Route path = "spot/:beachName" element = {<BeachDetail/>}/> //anything that comes after spot is saved as a parameter named beachName and the element bit means make this parameter available to the beach component.
+          <Route path="*" element={<NotFound/>} />
         </Route>
       </Routes>
     </BrowserRouter>
--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,13 +1,17 @@
 import "../App.css";
 
 export function NavBar() {
+    const handleClick = (e: React.MouseEvent<HTMLAnchorElement>) => { //temporary coming soon, sattelite map not in v1
+        e.preventDefault();
+        alert("Coming soon!");
+    };
     return (
         <>
         <div className = "nav-bar">
             <nav>
                 <ul className = "nav-bar-links">
                 <li><a href = "/">Home</a></li>
-                <li><a href = "/satellite-map">Satellite Map</a></li>
+                <li><a href = "/satellite-map" onClick = {handleClick}>Satellite Map</a></li>
                 <li><a href = "/how-it-works">How It Works</a></li>
                 <li><a href = "/about">About</a></li>
                 </ul>
