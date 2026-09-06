109e467ea52151e3ee7aa6b20df0511e51f609ae
Marcus Connelly <marcus.connelly@hotmail.com>
2026-09-06 21:29:20 +0100

    typo fixed

--- frontend/src/pages/HowItWorks.tsx
+++ frontend/src/pages/HowItWorks.tsx
@@ -8,7 +8,7 @@ export default function HowItWorks() {
                 <h2>Search:</h2>
                 <p>Use the search bar to find any surfing spot in the world.</p>
                 <h2>Locate & Fetch Conditions:</h2>
-                <p>SurfSpot finds the spot's exact coordinates, then gather the current conditions of that spot using external APIs.</p>
+                <p>SurfSpot finds the spot's exact coordinates, then gathers the current conditions of that spot using external APIs.</p>
                 <h2>Analyse:</h2>
                 <p>Finally, SurfSpot calculates a 1-10 rating for the spot via our algorithm that evaluates wave height, period, water temperature, wind speed, and weather conditions. Then SurfSpot provides reasoning for that score, including a breakdown of what is good and what to watch out for.</p>
             </div>
