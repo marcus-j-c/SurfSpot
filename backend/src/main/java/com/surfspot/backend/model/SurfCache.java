65fb3e8dbf011087cceb1b75b264729500c4c5d8
Marcus Connelly <marcus.connelly@hotmail.com>
2026-09-13 22:34:41 +0100

    created surf_cache table in surfspot_db

---
+++ backend/src/main/java/com/surfspot/backend/model/SurfCache.java
@@ -0,0 +1,10 @@
+package com.surfspot.backend.model;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity /* treat this java class as a postgres database table */
+@Table(name = "surf_cache") /* name the database */
+public class SurfCache {
+}
